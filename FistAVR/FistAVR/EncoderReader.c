a8580ab36166b425acd64b42af9d5627241701a5
Kajabo <kajabo.tempo@gmail.com>
2013-08-20 18:59:15 -0700

    Updated EncoderReader.c to respond to encoder input with LED.

--- FistAVR/FistAVR/EncoderReader.c
+++ FistAVR/FistAVR/EncoderReader.c
@@ -22,27 +22,35 @@ int read_encoder(void)
 
 
 volatile int16_t encoder_value = 0;
+volatile uint8_t oldEncoderPins = -1; //this large value will cause the variable to be initialized only in the ISR
+
 
 ISR(PCINT0_vect)
 {
 	// ISR code to execute here
-	//uint8_t led_pin = PB4;
-	//encoder_value = 200;
 
-	uint8_t button0 = 0;
-	uint8_t button1 = 0;
-
-	button0 = PINB & (1<<PB0);
-	if (button0)
-	{
-		//PORTB |= 1 << led_pin;              // LED on
-		encoder_value += 10;
-	}
+	uint8_t newEncoderPins = PINB & (1<<PB0 | 1<<PB1); //mask pins to get 0 and 1 values
 	
-	button1 = PINB & (1<<PB1);
-	if(button1)
-	{
-		//PORTB &= ~( 1 << led_pin );         // LED off
-		encoder_value -= 10;
+	switch(oldEncoderPins) { //Possible states: 00 01 11 10
+		case(0): // 00
+		if(newEncoderPins == (1<<0)) { ++encoder_value; }//10->00
+		if(newEncoderPins == (1<<1)) { --encoder_value; }//01->00
+		break;
+		case((1<<1)): // 01
+		if(newEncoderPins == 0) { ++encoder_value; }//00->01
+		if(newEncoderPins == ((1<<0)|(1<<1))) { --encoder_value; }//11->01
+		break;
+		case(((1<<0)|(1<<1))): // 11
+		if(newEncoderPins == (1<<1)) { ++encoder_value; }//01->11
+		if(newEncoderPins == (1<<0)) { --encoder_value; }//10->11
+		break;
+		case((1<<0)): // 10
+		if(newEncoderPins == ((1<<0)|(1<<1))) { ++encoder_value; }//11->10
+		if(newEncoderPins == 0) { --encoder_value; }//00->10
+		break;
 	}
+		
+	oldEncoderPins = newEncoderPins;	
+	
+	
 }
